65d0975d5cf31a315f973a0e8e85e426c72a6b75
Sagar Mopagar <sagar.mopagar@sap.com>
2020-09-29 19:44:16 +0530

    Maven Surefire Plugin Demo Code - testng group, maven system variables

--- src/test/java/SmokeTest/AppleTest.java
+++ src/test/java/SmokeTest/AppleTest.java
@@ -5,9 +5,22 @@ import org.testng.annotations.Test;
 public class AppleTest {
 
 	
-	@Test
+	@Test(groups = {"version1","version2"})
 	public void appleTest() {
 		System.out.println("Testing apples");
+		
+		//Reading System Property Variable from pom.xml
+		String BrowserOfChoice = System.getProperty("BrowserOfChoice");
+		System.out.println("You have chosen to run your tests in \""+BrowserOfChoice+"\"");
+		
+		//Fetch Project Build Directory:
+		String BuildDirectory = System.getProperty("BuildDirectory");
+		System.out.println(BuildDirectory);
+	}
+
+	@Test(groups= {"version3"})
+	public void additionalTest() {
+		System.out.println("Gets executed if you specify group = version3 in maven xml");
 	}
 	
 }
