1b9d44d0f1e3eb4ab21218dbd7cb44ee4a40571f
Sagar Mopagar <sagar.mopagar@sap.com>
2020-09-29 18:47:37 +0530

    Maven Surefire Plugin Demo Code

---
+++ src/test/java/SanityTest/OrangeTest.java
@@ -0,0 +1,12 @@
+package SanityTest;
+
+import org.testng.annotations.Test;
+
+public class OrangeTest {
+
+	@Test
+	public void testOranges() {
+		System.out.println("Test Oranges");
+	}
+	
+}
---
+++ src/test/java/SmokeTest/AppleTest.java
@@ -0,0 +1,13 @@
+package SmokeTest;
+
+import org.testng.annotations.Test;
+
+public class AppleTest {
+
+	
+	@Test
+	public void appleTest() {
+		System.out.println("Testing apples");
+	}
+	
+}
